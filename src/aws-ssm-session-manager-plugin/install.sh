09a1ecb798d11d1044144e20c29a0157dae97724
Yuchan Lee <lasuillard@gmail.com>
2024-09-15 18:16:39 +0900

    Add feature: OpenAPI Generator CLI (#3)

--- src/aws-ssm-session-manager-plugin/install.sh
+++ src/aws-ssm-session-manager-plugin/install.sh
@@ -4,6 +4,8 @@ set -e
 
 ARCH=$(uname -m)
 
+echo "Preparing to download plugin for $ARCH"
+
 case $ARCH in
     x86_64)
         download_url='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb'
@@ -20,8 +22,13 @@ case $ARCH in
         ;;
 esac
 
+echo "Downloading plugin from $download_url"
+
 apt-get update && apt-get install -y curl
 
 curl -sL "$download_url" -o /tmp/session-manager-plugin.deb
+
+echo "Installing plugin"
+
 dpkg -i /tmp/session-manager-plugin.deb
 rm /tmp/session-manager-plugin.deb
--- test/aws-ssm-session-manager-plugin/test.sh
+++ test/aws-ssm-session-manager-plugin/test.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# shellcheck source=/dev/null
 source dev-container-features-test-lib
 
 check "verify session manager plugin installation" session-manager-plugin --version | grep -Eo '[0-9\.]+'
