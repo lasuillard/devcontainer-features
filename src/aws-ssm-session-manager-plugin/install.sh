376b37f96dba364db76f7766736ad67553ce8c03
Yuchan Lee <lasuillard@gmail.com>
2024-09-26 11:33:00 +0000

    style: Reformat codes

--- src/aws-ssm-session-manager-plugin/install.sh
+++ src/aws-ssm-session-manager-plugin/install.sh
@@ -7,16 +7,16 @@ ARCH=$(dpkg --print-architecture)
 echo "Preparing to download plugin for $ARCH"
 
 case $ARCH in
-    amd64)
-        download_url='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb'
-        ;;
-    arm64)
-        download_url='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_arm64/session-manager-plugin.deb'
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
+amd64)
+  download_url='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb'
+  ;;
+arm64)
+  download_url='https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_arm64/session-manager-plugin.deb'
+  ;;
+*)
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+  ;;
 esac
 
 if ! command -v curl; then
--- src/openapi-generator-cli/install.sh
+++ src/openapi-generator-cli/install.sh
@@ -15,22 +15,22 @@ if [ "$INSTALL_JAVA" = 'true' ]; then
 fi
 
 if [ "$VERSION" = 'latest' ]; then
-    echo "Resolving latest version of OpenAPI Generator CLI"
-    VERSION=$(
-        curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/maven-metadata.xml \
-            | grep -Eo '<latest>(.+)</latest>' \
-            | sed -E 's/<latest>(.+)<\/latest>/\1/'
-    )
+  echo "Resolving latest version of OpenAPI Generator CLI"
+  VERSION=$(
+    curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/maven-metadata.xml |
+      grep -Eo '<latest>(.+)</latest>' |
+      sed -E 's/<latest>(.+)<\/latest>/\1/'
+  )
 fi
 
 echo "Installing OpenAPI Generator CLI with version ${VERSION}"
 
 curl -o /usr/local/lib/openapi-generator-cli.jar \
-    "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar"
+  "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar"
 
 echo "Creating wrapper script for OpenAPI Generator CLI"
 
-cat <<EOF > /usr/local/bin/openapi-generator-cli
+cat <<EOF >/usr/local/bin/openapi-generator-cli
 #!/bin/sh
 
 java -jar /usr/local/lib/openapi-generator-cli.jar "\$@"
