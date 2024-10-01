d8a8a4f836c885a86f84fcba3e4532d4ebdeebaa
Yuchan Lee <lasuillard@gmail.com>
2024-10-01 01:47:38 +0000

    fix: Wrong assertion

--- test/openapi-generator-cli/latest-with-java.sh
+++ test/openapi-generator-cli/latest-with-java.sh
@@ -5,7 +5,7 @@ set -e
 # shellcheck source=/dev/null
 source dev-container-features-test-lib
 
-check "verify Java installation" java --version | grep -o 'openjdk .+$'
+check "verify Java installation" java --version | grep -E 'openjdk .+$'
 check "verify OpenAPI Generator CLI installation" openapi-generator-cli --version | grep -E '^openapi-generator-cli .+$'
 
 reportResults
