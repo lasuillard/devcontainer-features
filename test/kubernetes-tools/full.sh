837ebe0702eb9677f1286f15e2bada2a16a6134d
Yuchan Lee <lasuillard@gmail.com>
2024-11-17 14:23:14 +0000

    test: Fix test

--- test/kubernetes-tools/full.sh
+++ test/kubernetes-tools/full.sh
@@ -8,6 +8,6 @@ source dev-container-features-test-lib
 check "verify kubectl installation" kubectl version --client | grep -E 'Client Version: v.+'
 check "verify Krew installation" krew version | grep -E 'GitTag\s+v.+'
 check "verify k9s installation" k9s version --short | grep -E 'Version\s+v.+'
-check "verify Helm installation" helm version --short | grep 'v.+'
+check "verify Helm installation" helm version --short | grep -E 'v.+'
 
 reportResults
